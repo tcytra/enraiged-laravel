ce3558bcd3d4f09cbe45c68c4cf90b5a2e1dc888
Todd Cytra <tcytra@gmail.com>
2022-04-08 11:01:35 -0700

    Minor: Moved FlashMessages flashable indexes into class attribute

--- app/Auth/Services/FlashMessages.php
+++ app/Auth/Services/FlashMessages.php
@@ -6,17 +6,22 @@ use Illuminate\Http\Request;
 
 class FlashMessages
 {
+    /** @var  array  The flashable session indexes. */
+    protected $flashable = [
+        'data', 'message', 'status', 'success',
+    ];
+
     /**
      *  Handle a request for the flashed session data.
      *
      *  @param  \Illuminate\Http\Request  $request
-     *  @return array|null
+     *  @return array
      */
-    public function handle(Request $request)
+    public function handle(Request $request): array
     {
         $flash = [];
 
-        foreach (['data', 'message', 'status', 'success'] as $each) {
+        foreach ($this->flashable as $each) {
             if ($request->session()->has($each)) {
                 $flash[$each] = $request->session()->get($each);
                 $request->session()->forget($each);
