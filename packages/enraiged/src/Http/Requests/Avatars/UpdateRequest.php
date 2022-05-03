fb769947e84f1852b8e61e36d74d46815ff36d44
Todd Cytra <tcytra@gmail.com>
2022-05-03 10:22:50 -0700

    Bugfix: Corrected namespace issue in the enraiged avatar requests

--- packages/enraiged/src/Http/Requests/Avatars/UpdateRequest.php
+++ packages/enraiged/src/Http/Requests/Avatars/UpdateRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\Avatars;
+namespace Enraiged\Http\Requests\Avatars;
 
 use Enraiged\Avatars\Traits\Validators\AvatarColorValidator;
 use Enraiged\Forms\Requests\FormRequest;
--- packages/enraiged/src/Http/Requests/Avatars/UploadRequest.php
+++ packages/enraiged/src/Http/Requests/Avatars/UploadRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\Avatars;
+namespace Enraiged\Http\Requests\Avatars;
 
 use Enraiged\Avatars\Traits\Validators\AvatarFileValidator;
 use Enraiged\Forms\Requests\FormRequest;
