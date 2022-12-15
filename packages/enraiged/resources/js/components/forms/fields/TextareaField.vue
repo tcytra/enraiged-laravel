08834dea14d930705d5caf71a210e2106acebaf6
Todd Cytra <tcytra@gmail.com>
2022-12-15 04:39:17 -0800

    Bugfix: Ensure TextareaField updated the form correctly

--- packages/enraiged/resources/js/components/forms/fields/TextareaField.vue
+++ packages/enraiged/resources/js/components/forms/fields/TextareaField.vue
@@ -13,7 +13,8 @@
                 :disabled="disabled"
                 :id="id"
                 :placeholder="placeholder"
-                :rows="field.rows || rows"/>
+                :rows="field.rows || rows"
+                @update:modelValue="update"/>
             <div class="error p-error" v-if="error">
                 <i class="pi pi-exclamation-circle" v-tooltip.top="error"></i>
                 <span class="message">{{ error }}</span>
