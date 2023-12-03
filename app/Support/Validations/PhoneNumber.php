633478b7bd142619f6da6e32f3fd6a94c988f707
Todd Cytra <todd.cytra@oracast.com>
2023-12-03 10:58:40 -0800

    Minor: Improved the PhoneNumber validation

--- app/Support/Validations/PhoneNumber.php
+++ app/Support/Validations/PhoneNumber.php
@@ -7,6 +7,9 @@ use Illuminate\Contracts\Validation\ValidationRule;
 
 class PhoneNumber implements ValidationRule
 {
+    /** @var  string  The validation failure message. */
+    protected string $message = 'The :attribute field must be a valid phone number.';
+
     /**
      *  Run the validation rule.
      *
@@ -14,10 +17,14 @@ class PhoneNumber implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
+        if (!preg_match('/^([0-9\s\-\+\(\)]*)$/', $value) || strlen($value) < 10) {
+            $fail($this->message);
+        }
+
         $number = preg_replace('/[^\d]/', '', $value);
 
         if (!preg_match('/^(\d{10}|\d{11})$/', $number)) {
-            $fail('The :attribute field must be a valid phone number.');
+            $fail($this->message);
 
             // $fail('validation.phonenumber')->translate();
         }
