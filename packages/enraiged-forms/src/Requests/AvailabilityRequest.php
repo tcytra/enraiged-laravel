a0088db43024f5bdfd63873a988e1cea89da77bf
Todd Cytra <todd.cytra@oracast.com>
2024-04-29 14:04:03 -0700

    Minor: Added columns method,property to enraiged AvailabilityRequest

--- packages/enraiged-forms/src/Requests/AvailabilityRequest.php
+++ packages/enraiged-forms/src/Requests/AvailabilityRequest.php
@@ -6,6 +6,12 @@ use Enraiged\Forms\Requests\FormRequest as Request;
 
 class AvailabilityRequest extends Request
 {
+    /** @var  array  The columns selected for the available options. */
+    protected $columns = [
+        'id',
+        'name',
+    ];
+
     /** @var  array  The custom error messages for the validations. */
     protected $messages = [
         'limit.integer' => 'The limit parameter must be an integer',
@@ -17,4 +23,16 @@ class AvailabilityRequest extends Request
         'limit' => 'nullable|integer',
         'search' => 'nullable|string',
     ];
+
+    /**
+     *  Get the columns to select for the available options.
+     *
+     *  @return array
+     */
+    public function columns()
+    {
+        return property_exists($this, 'columns')
+            ? $this->columns
+            : [];
+    }
 }
