4c2f5cb09363015cb846468656328d0380f09996
Todd Cytra <todd.cytra@oracast.com>
2025-03-05 12:30:51 -0800

    Install: Initial install of Laravel Breeze 2.3 (w/ inertiajs, ziggy)

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
         }),
-        tailwindcss(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
 });
