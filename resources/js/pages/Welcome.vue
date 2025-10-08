10a451b05cef155fad2917aaac818cebb41d3b1c
Todd Cytra <tcytra@gmail.com>
2025-10-08 08:59:19 -0700

    Minor: Cleanup in the laravel welcome page

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,37 +1,5 @@
-<script setup>
-import { Head, Link } from '@inertiajs/vue3';
-
-defineProps({
-    allowLogin: {
-        type: Boolean,
-    },
-    allowRegistration: {
-        type: Boolean,
-    },
-    enraigedVersion: {
-        type: String,
-        required: true,
-    },
-    laravelVersion: {
-        type: String,
-        required: true,
-    },
-    phpVersion: {
-        type: String,
-        required: true,
-    },
-});
-
-function handleImageError() {
-    document.getElementById('screenshot-container')?.classList.add('!hidden');
-    document.getElementById('docs-card')?.classList.add('!row-span-1');
-    document.getElementById('docs-card-content')?.classList.add('!flex-row');
-    document.getElementById('background')?.classList.add('!hidden');
-}
-</script>
-
 <template>
-    <Head title="Welcome" />
+    <html-head title="Welcome" />
     <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
         <img class="absolute -left-20 top-0 max-w-[877px]" id="background"
             src="https://laravel.com/assets/img/welcome/background.svg" />
@@ -47,24 +15,24 @@ function handleImageError() {
                         </svg>
                     </div>
                     <nav v-if="allowLogin || allowRegistration" class="-mx-3 flex flex-1 justify-end">
-                        <Link v-if="$page.props.auth"
+                        <html-link v-if="$page.props.auth"
                             :href="route('dashboard')"
                             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
                             Dashboard
-                        </Link>
+                        </html-link>
 
                         <template v-else>
-                            <Link v-if="allowLogin"
+                            <html-link v-if="allowLogin"
                                 :href="route('login')"
                                 class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
                                 Log in
-                            </Link>
+                            </html-link>
 
-                            <Link v-if="allowRegistration"
+                            <html-link v-if="allowRegistration"
                                 :href="route('register')"
                                 class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
                                 Register
-                            </Link>
+                            </html-link>
                         </template>
                     </nav>
                 </header>
@@ -277,3 +245,35 @@ function handleImageError() {
         </div>
     </div>
 </template>
+
+<script setup>
+import { Head as HtmlHead, Link as HtmlLink } from '@inertiajs/vue3';
+
+defineProps({
+    allowLogin: {
+        type: Boolean,
+    },
+    allowRegistration: {
+        type: Boolean,
+    },
+    enraigedVersion: {
+        type: String,
+        required: true,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+
+function handleImageError() {
+    document.getElementById('screenshot-container')?.classList.add('!hidden');
+    document.getElementById('docs-card')?.classList.add('!row-span-1');
+    document.getElementById('docs-card-content')?.classList.add('!flex-row');
+    document.getElementById('background')?.classList.add('!hidden');
+}
+</script>
