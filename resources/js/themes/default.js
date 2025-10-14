2b84739b1e6b04df06f4f2227f9da3e40b957b3b
Todd Cytra <tcytra@gmail.com>
2025-10-14 16:54:10 -0700

    Minor: Cleanup in the resources/js/themes files

--- resources/js/themes/default.js
+++ resources/js/themes/default.js
@@ -1,8 +1,57 @@
-import pt from './default/passthrough';
-import theme from './default/theme';
+import { definePreset } from '@primeuix/themes';
+import { palette } from '@/themes/palette';
+import Aura from '@primevue/themes/aura'; // options: Aura, Lara, Material, Nora
+
+const {
+    currentPrimary,
+    currentSurface,
+    darkModeSelector,
+    primaryColors,
+    surfaceColors,
+} = palette();
+
+const defaultPreset = definePreset(Aura, {
+    semantic: {
+        primary: primaryColors[currentPrimary.value],
+        colorScheme: {
+            dark: {
+                surface: surfaceColors[currentSurface.value],
+            },
+            light: {
+                surface: surfaceColors[currentSurface.value],
+            },
+        },
+        formField: {
+            //paddingX: '0.75rem',
+            //paddingY: '0.25rem',
+            //etc
+        },
+    },
+    components: {
+        button: {
+        },
+    },
+});
 
 export default {
-    pt,
-    theme,
+    pt: {
+        /*
+        card: {
+            header: {
+                class: 'card-header-test',
+            },
+        },
+        */
+    },
+    theme: {
+        preset: defaultPreset,
+        options: {
+            cssLayer: {
+                name: 'primevue',
+                order: 'theme, base, primevue, utilities',
+            },
+            darkModeSelector,
+        },
+    },
     ripple: true,
 };
