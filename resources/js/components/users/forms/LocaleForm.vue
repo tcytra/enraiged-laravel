aaba24b4f328b951a7bc0563645426cd615adea9
Todd Cytra <tcytra@gmail.com>
2025-11-27 10:15:11 -0800

    Bugfix: Correct issues in the LocaleForm.vue component

--- resources/js/components/users/forms/LocaleForm.vue
+++ resources/js/components/users/forms/LocaleForm.vue
@@ -1,25 +1,14 @@
 <template>
     <form class="form grid grid-cols-1 mt-1" @submit.prevent="submit">
-        <hidden-field id="locale"
-            :field="{
-            }"
-            :form="form">
-            <template #content="field">
-                <div class="control field">
-                    <label class="block label">
-                        {{ field.label }}
-                    </label>
-                    <div class="flex flex-col lg:flex-row lg:justify-between w-full">
-                        <secondary-button class="!px-6 mb-5 lg:mb-0" v-for="(name, locale) in locales"
-                            :disabled="form.processing || form.locale === locale"
-                            :key="locale"
-                            @click="form.locale = locale; ai18n(locale); submit()">
-                            {{ name }}
-                        </secondary-button>
-                    </div>
-                </div>
-            </template>
-        </hidden-field>
+        <hidden-field :field="{ id: 'locale' }" :form="form" />
+        <div class="flex flex-col lg:flex-row lg:justify-between mt-3 w-full">
+            <primary-button class="!px-6 mb-5 lg:mb-0" v-for="(name, locale) in locales"
+                :disabled="form.processing || form.locale === locale"
+                :key="locale"
+                @click="set(locale)">
+                {{ name }}
+            </primary-button>
+        </div>
     </form>
 </template>
 
@@ -28,7 +17,7 @@ import { inject } from 'vue';
 import { useForm } from '@inertiajs/vue3';
 import { useLocales } from '@/handlers/locales';
 import HiddenField from '@/components/forms/fields/HiddenField.vue';
-import SecondaryButton from '@/components/ui/buttons/SecondaryButton.vue';
+import PrimaryButton from '@/components/ui/buttons/SecondaryButton.vue';
 
 const props = defineProps({
     alert: {
@@ -46,13 +35,24 @@ const props = defineProps({
 });
 
 const { ai18n, i18n } = useLocales();
+
 const { meta } = inject('app');
+
 const locales = meta.value.locales;
+
 const user = props.user;
 
 const form = useForm({
     locale: props.user.locale,
 });
+
+const set = (locale) => {
+    if (props.isMyProfile) {
+        ai18n(locale);
+    }
+    form.locale = locale;
+    submit();
+};
 
 const submit = () => {
     form.patch(route('users.update', {user: user.id}));
