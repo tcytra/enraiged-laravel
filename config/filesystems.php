08a35046b4cb54f42a6c2b79e51875c19a661a11
Todd Cytra <tcytra@gmail.com>
2025-11-27 09:37:20 -0800

    Minor: Added the temp filesystem driver to the laravel config

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,6 +47,14 @@ return [
             'report' => false,
         ],
 
+        'temp' => [
+            'driver' => 'local',
+            'root' => storage_path('app/temp'),
+            'serve' => true,
+            'throw' => false,
+            'report' => false,
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
