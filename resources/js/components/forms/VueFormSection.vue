3767d417a6110bd60fa7cfa51d6ed3f2e6bb3028
Todd Cytra <tcytra@gmail.com>
2026-02-04 08:58:34 -0800

    Minor: Improved slot handling of the VueFormSection component

--- resources/js/components/forms/VueFormSection.vue
+++ resources/js/components/forms/VueFormSection.vue
@@ -1,41 +1,42 @@
 <template>
-    <section class="section" :class="section.class">
-        <slot :name="id" v-if="section.custom" v-bind="{ creating, section, form, updating }"/>
-        <primevue-card v-else>
-            <template #header>
-                <header class="header" v-if="section.heading"
-                    :class="[ section.heading.class ]">
-                    <span class="text">{{ i18n(section.heading.body || section.heading) }}</span>
-                </header>
-            </template>
-            <template #content>
-                <div class="precontent mb-3" v-if="section.precontent"
-                    :class="[ section.precontent.class ]">
-                    {{ i18n(section.precontent.body || section.precontent) }}
-                </div>
-                <vue-form-fields
-                    :class="section.class"
-                    :creating="creating"
-                    :fields="section.fields"
-                    :form="form"
-                    :invalid="error"
-                    :template="template"
-                    :updating="updating">
-                    <template v-for="(_, slot) of $slots" v-slot:[slot]="scope">
-                        <slot :name="slot" v-bind="scope"/>
-                    </template>
-                </vue-form-fields>
-                <div class="postcontent" v-if="section.postcontent"
-                    :class="[ section.postcontent.class ]">
-                    {{ i18n(section.postcontent.body || section.postcontent) }}
-                </div>
-                <div class="p-message p-message-error error section-error" v-if="error">
-                    <div class="p-message-wrapper py-1 px-3">
-                        <div class="p-message-text p-message-text">{{ error }}</div>
+    <section class="section">
+        <slot :name="id" v-bind="{ creating, section, form, updating }">
+            <primevue-card>
+                <template #header>
+                    <header class="header" v-if="section.heading"
+                        :class="[ section.heading.class ]">
+                        <span class="text">{{ i18n(section.heading.body || section.heading) }}</span>
+                    </header>
+                </template>
+                <template #content>
+                    <div class="precontent mb-3" v-if="section.precontent"
+                        :class="[ section.precontent.class ]">
+                        {{ i18n(section.precontent.body || section.precontent) }}
                     </div>
-                </div>
-            </template>
-        </primevue-card>
+                    <vue-form-fields
+                        :class="section.class"
+                        :creating="creating"
+                        :fields="section.fields"
+                        :form="form"
+                        :invalid="error"
+                        :template="template"
+                        :updating="updating">
+                        <template v-for="(_, slot) of $slots" v-slot:[slot]="scope">
+                            <slot :name="slot" v-bind="scope" />
+                        </template>
+                    </vue-form-fields>
+                    <div class="postcontent" v-if="section.postcontent"
+                        :class="[ section.postcontent.class ]">
+                        {{ i18n(section.postcontent.body || section.postcontent) }}
+                    </div>
+                    <div class="p-message p-message-error error section-error" v-if="error">
+                        <div class="p-message-wrapper py-1 px-3">
+                            <div class="p-message-text p-message-text">{{ error }}</div>
+                        </div>
+                    </div>
+                </template>
+            </primevue-card>
+        </slot>
     </section>
 </template>
 
@@ -85,6 +86,9 @@ export default {
         },
         error() {
             return this.form && this.form.errors ? this.form.errors[this.id] : null;
+        },
+        i18n() {
+            return i18n;
         },
     },
 };    
