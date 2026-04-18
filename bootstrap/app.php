cbe88389c11a16bb163b59489be5fa66a4a644da
Todd Cytra <tcytra@gmail.com>
2026-04-18 12:17:19 -0700

    Minor: Added channels to the app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
+        channels: __DIR__.'/../routes/channels.php',
         health: '/up',
     )
 
