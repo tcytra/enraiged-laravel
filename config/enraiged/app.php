b04e76d4cede2761fd1872891343a5b0f94c1a2e
Todd Cytra <tcytra@gmail.com>
2026-05-01 00:08:09 -0700

    Update: Added system_user config definition

--- config/enraiged/app.php
+++ config/enraiged/app.php
@@ -30,6 +30,22 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | System User
+    |--------------------------------------------------------------------------
+    | 
+    | Provide the email of a user account that will be used for otherwise
+    | untracked system operations, such as queued jobs, activity logs, etc.
+    |
+    | It is recommended to seed this user as hidden and protected to prevent it
+    | from being removed or altered,
+    |
+    | The default value is 'administrator@enraiged.local'.
+    |
+    */
+    'system_user' => env('SYSTEM_USER', 'administrator@enraiged.local'),
+
+    /*
+    |--------------------------------------------------------------------------
     | Timezone
     |--------------------------------------------------------------------------
     | 
