cbc914ce7f3815c83ea5f87be240f94cbde85032
Todd Cytra <tcytra@gmail.com>
2026-05-01 00:04:02 -0700

    Update: Added blade layout,page for maintenance mode (artisan down)

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.errors')
+
+@section('title', 'Service Unavailable')
+
+@section('message')
+    {{ env('APP_NAME') }} is undergoing a scheduled update.
+    <br><br>
+    The service will return shortly.
+@endsection
